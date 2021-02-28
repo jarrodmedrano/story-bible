43f53966572aae79da6184fb74b32bd5b7569d09
Jarrod Medrano <jmedran@gmail.com>
2021-02-28 14:53:46 -0700

    image upload to cloudinary

--- next.config.js
+++ next.config.js
@@ -2,4 +2,10 @@ module.exports = {
   images: {
     domains: ['avatars1.githubusercontent.com'],
   },
+  env: {
+    CLOUDINARY_URL: process.env.CLOUDINARY_URL,
+    NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+    NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET: process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET,
+    NEXT_PUBLIC_CLOUDINARY_KEY: process.env.NEXT_PUBLIC_CLOUDINARY_KEY,
+  },
 }
---
+++ src/pages/api/files/uploadFileApi.ts
@@ -0,0 +1,26 @@
+import axios from 'axios'
+
+export interface IUploadDocumentsApi {
+  (formData: FormData): Promise<IUploadDocumentsApiResponse>
+}
+
+export interface IUploadDocumentsApiResponse {
+  success: boolean
+  data: any
+  moved?: string
+  errors: {
+    code: string
+    category: string
+    message: string
+    data: any[]
+  }[]
+}
+
+export const uploadDocumentsApi: IUploadDocumentsApi = async (request: FormData) => {
+  const response = await axios({
+    method: 'POST',
+    data: request,
+    url: `https://api.cloudinary.com/v1_1/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/upload`,
+  })
+  return response.data
+}
