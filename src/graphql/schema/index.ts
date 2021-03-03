a129858f2492067b1d07f5d4441993933088503a
Jarrod Medrano <jmedran@gmail.com>
2021-03-03 07:09:01 -0700

    fix typegen

--- src/graphql/schema/index.ts
+++ src/graphql/schema/index.ts
@@ -17,10 +17,14 @@ export const baseSchema = makeSchema({
     }),
   ],
   outputs: {
-    // typegen: path.join(__dirname, '../node_modules/@types/nexus-typegen/index.d.ts'),
-    typegen: path.join(process.cwd(), 'src/graphql/schema/nexus-typegen.ts'),
-    schema: path.join(process.cwd(), 'src/graphql/schema/schema.graphql'),
+    typegen: path.join(__dirname, 'nexus-plugin-prisma-typegen.ts'),
+    schema: path.join(__dirname, 'schema.graphql'),
   },
+  // outputs: {
+  //   // typegen: path.join(__dirname, '../node_modules/@types/nexus-typegen/index.d.ts'),
+  //   typegen: path.join(process.cwd(), 'src/graphql/schema/nexus-typegen.ts'),
+  //   schema: path.join(process.cwd(), 'src/graphql/schema/schema.graphql'),
+  // },
   shouldGenerateArtifacts: process.env.NODE_ENV === 'development',
 })
 
