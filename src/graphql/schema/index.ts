3460ffba3133817c4d79cc264ed376f823e1b182
Jarrod Medrano <jmedran@gmail.com>
2021-03-02 22:04:08 -0700

    change dir

--- src/graphql/schema/index.ts
+++ src/graphql/schema/index.ts
@@ -17,9 +17,9 @@ export const baseSchema = makeSchema({
     }),
   ],
   outputs: {
-    typegen: path.join(__dirname, '../node_modules/@types/nexus-typegen/index.d.ts'),
-    // typegen: path.join(process.cwd(), 'src/graphql/schema/nexus-typegen.ts'),
-    schema: path.join(process.cwd(), 'src/graphql/schema/schema.graphql'),
+    // typegen: path.join(__dirname, '../node_modules/@types/nexus-typegen/index.d.ts'),
+    typegen: path.join(__dirname, './nexus-typegen.ts'),
+    schema: path.join(__dirname, './schema.graphql'),
   },
   shouldGenerateArtifacts: process.env.NODE_ENV === 'development',
 })
