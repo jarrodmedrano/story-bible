02915c6dda38728926dfa4fd97df7bb674dfdb4a
Jarrod Medrano <jmedran@gmail.com>
2023-01-21 14:14:40 -0700

    check on session

--- src/graphql/permissions/rules/isAuthenticated.ts
+++ src/graphql/permissions/rules/isAuthenticated.ts
@@ -3,6 +3,7 @@ import { rule } from 'graphql-shield'
 
 export const isAuthenticated = rule({ cache: 'contextual' })(async (_parent, _args, { req }, _info) => {
   const session = await getSession({ req })
+  console.log('session', session)
   return Boolean(session)
   //return true
 })
