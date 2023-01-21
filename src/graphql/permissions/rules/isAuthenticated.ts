633fdd62c343944ee198d766152402d0c60447f7
Jarrod Medrano <jmedran@gmail.com>
2023-01-21 14:42:31 -0700

    try this

--- src/graphql/permissions/rules/isAuthenticated.ts
+++ src/graphql/permissions/rules/isAuthenticated.ts
@@ -2,8 +2,7 @@ import { getSession } from 'next-auth/client'
 import { rule } from 'graphql-shield'
 
 export const isAuthenticated = rule({ cache: 'contextual' })(async (_parent, _args, { req }, _info) => {
-  const session = await getSession({ req })
-  // console.log('session', session)
+  //const session = await getSession({ req })
   // return Boolean(session)
   return true
 })
